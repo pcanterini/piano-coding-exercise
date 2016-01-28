34e9cc5e477847ed546197c88cfdc4f518607ab4
Pedro Canterini <pedro@pcanterini.com>
2016-01-27 18:44:02 -0800

    updated key to stateless component

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 
 const propTypes = {
@@ -6,45 +6,68 @@ const propTypes = {
   keyHandler: PropTypes.func.isRequired,
 };
 
-class Key extends Component {
+const Key = ({ pianoKey, keyHandler }) => {
+  const keyClassNames = classNames({
+    key: true,
+    active: pianoKey.isActive,
+    [`key-${pianoKey.type}`]: true,
+  });
 
-  constructor(props) {
-    super(props);
+  const _onMouseDown = () => keyHandler(pianoKey.label);
+  const _onMouseUp = () => keyHandler();
 
-    // this._onMouseDown = this._onMouseDown.bind(this);
-    this._onMouseUp = this._onMouseUp.bind(this);
-  }
+  return (
+    <div
+      className={keyClassNames}
+      onMouseDown={_onMouseDown}
+      onMouseUp={_onMouseUp}
+    >
+      <span className="label">{pianoKey.label}</span>
+    </div>
+  );
+};
 
-  _onMouseDown = () => this.props.keyHandler(this.props.pianoKey.label);
 
-  // _onMouseDown() {
-  //   this.props.keyHandler(this.props.pianoKey.label);
-  // }
-
-  _onMouseUp() {
-    this.props.keyHandler();
-  }
-
-  render() {
-    const pianoKey = this.props.pianoKey;
-    const keyClassNames = classNames({
-      key: true,
-      active: pianoKey.isActive,
-      [`key-${pianoKey.type}`]: true,
-    });
-
-    return (
-      <div
-        className={keyClassNames}
-        onMouseDown={this._onMouseDown}
-        onMouseUp={this._onMouseUp}
-      >
-        <span className="label">{pianoKey.label}</span>
-      </div>
-    );
-  }
-
-}
+// class Key extends Component {
+//
+//   constructor(props) {
+//     super(props);
+//
+//     // this._onMouseDown = this._onMouseDown.bind(this);
+//     // this._onMouseUp = this._onMouseUp.bind(this);
+//   }
+//
+//   _onMouseDown = () => this.props.keyHandler(this.props.pianoKey.label);
+//   _onMouseUp = () => this.props.keyHandler();
+//
+//   // _onMouseDown() {
+//   //   this.props.keyHandler(this.props.pianoKey.label);
+//   // }
+//
+//   // _onMouseUp() {
+//   //   this.props.keyHandler();
+//   // }
+//
+//   render() {
+//     const pianoKey = this.props.pianoKey;
+//     const keyClassNames = classNames({
+//       key: true,
+//       active: pianoKey.isActive,
+//       [`key-${pianoKey.type}`]: true,
+//     });
+//
+//     return (
+//       <div
+//         className={keyClassNames}
+//         onMouseDown={this._onMouseDown}
+//         onMouseUp={this._onMouseUp}
+//       >
+//         <span className="label">{pianoKey.label}</span>
+//       </div>
+//     );
+//   }
+//
+// }
 
 Key.propTypes = propTypes;
 export default Key;
