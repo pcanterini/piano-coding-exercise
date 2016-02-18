58470e23e046ec690fd9a11785b5150f4282a90d
Pedro Canterini <pedro@pcanterini.com>
2016-02-17 21:42:54 -0800

    remove unused code

--- src/components/Key.js
+++ src/components/Key.js
@@ -28,48 +28,5 @@ const Key = ({ pianoKey, keyHandler }) => {
   );
 };
 
-// TODO: Delete commented code after stateless component is converted
-
-// class Key extends Component {
-//
-//   constructor(props) {
-//     super(props);
-//
-//     // this._onMouseDown = this._onMouseDown.bind(this);
-//     // this._onMouseUp = this._onMouseUp.bind(this);
-//   }
-//
-//   _onMouseDown = () => this.props.keyHandler(this.props.pianoKey.label);
-//   _onMouseUp = () => this.props.keyHandler();
-//
-//   // _onMouseDown() {
-//   //   this.props.keyHandler(this.props.pianoKey.label);
-//   // }
-//
-//   // _onMouseUp() {
-//   //   this.props.keyHandler();
-//   // }
-//
-//   render() {
-//     const pianoKey = this.props.pianoKey;
-//     const keyClassNames = classNames({
-//       key: true,
-//       active: pianoKey.isActive,
-//       [`key-${pianoKey.type}`]: true,
-//     });
-//
-//     return (
-//       <div
-//         className={keyClassNames}
-//         onMouseDown={this._onMouseDown}
-//         onMouseUp={this._onMouseUp}
-//       >
-//         <span className="label">{pianoKey.label}</span>
-//       </div>
-//     );
-//   }
-//
-// }
-
 Key.propTypes = propTypes;
 export default Key;
