23045958684192d644ac61ae9169903a891cffc7
Pedro Canterini <pedro@pcanterini.com>
2016-02-17 21:52:02 -0800

    simplify logger component

--- src/components/Logger.js
+++ src/components/Logger.js
@@ -1,27 +1,16 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 const propTypes = {
   loggerData: PropTypes.array.isRequired,
 };
 
-// TODO: Make it a stateless component
+const Logger = (props) => (
+  <div className="logger">
+    {props.loggerData.map((key, i) => {
+      return <span key={i}>{key}</span>;
+    })}
+  </div>
+);
 
-class Logger extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="logger">
-        {this.props.loggerData.map(function mapKeys(key, i) {
-          return <span key={i}>{key}</span>;
-        }, this)}
-      </div>
-    );
-  }
-
-}
 Logger.propTypes = propTypes;
 export default Logger;
