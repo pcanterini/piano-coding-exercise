8ccbf39f0c71a3f8a19d0149d6b1c18884fc0f1c
Pedro Canterini <pedro@pcanterini.com>
2016-02-22 18:49:48 -0800

    fix app tests

--- test/containers/App.spec.js
+++ test/containers/App.spec.js
@@ -5,17 +5,17 @@ import Piano from '../../src/components/Piano';
 
 describe('(Container) App', () => {
   it('renders main App container as a <div>', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<App onAppRender={()=>{}} />);
     expect(wrapper.type()).to.eql('div');
   });
 
   it('contains a header title', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<App onAppRender={()=>{}} />);
     expect(wrapper.find('.title-header')).to.have.length(1);
   });
 
   it('contains a piano component', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<App onAppRender={()=>{}} />);
     expect(wrapper.find(Piano).length >= 0).to.equal(true);
   });
 
